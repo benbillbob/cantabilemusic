ce58b2ab89689d65ebd54ff13d680bbc56b8827a
ben <ben@benbillbob.com>
2017-09-23 06:25:34 +0000

    Init

---
+++ mysite/_config.php
@@ -0,0 +1,17 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	'type' => 'MySQLDatabase',
+	'server' => 'localhost',
+	'username' => 'SSCC',
+	'password' => 'bUtb2U2RerLm3xTE',
+	'database' => 'SSCC',
+	'path' => ''
+);
+
+// Set the site locale
+i18n::set_locale('en_US');
